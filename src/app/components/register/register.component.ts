35b301a604be073665933f02084cae0f2fce59c0
Bhavana Ingole <Bhavanaingole456@gmail.com>
2021-01-11 17:47:56 +0530

    Updates in Register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  user = new User();
   constructor() { }
 
   ngOnInit() {
