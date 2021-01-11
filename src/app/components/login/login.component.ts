b6c99d7027382d3da9ec26e507ab9149f88494d7
Devashish Gupta <devashish.gupta1998@gmail.com>
2021-01-11 14:00:55 +0530

    Login updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/models/login';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  log=new Login;
 
   constructor() { }
 
   ngOnInit() {
   }
+  public login(){
 
+  }
 }
